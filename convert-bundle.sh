2904184c1ef46f56c5d8eafed918f7df2872c8cb
Rachel Seo <rachelseo@Rachels-MacBook-Air.local>
2024-09-21 21:14:15 -0400

    script #2 done

--- convert-bundle.sh
+++ convert-bundle.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
-clear
-wget https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
 
+# download URL
+wget -q https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
+
+# extract the tar.gz file
 tar -xzvf lab3-bundle.tar.gz
 
-awk '!/^[[:space:]]*$/' lab3-bundle.tsv
-tr '\t' ',' < file.tsv > lab3-bundle.csv
+# remove blank lines from TSV file & save to a new file
+awk '!/^[[:space:]]*$/' lab3_data.tsv > "cleaned_lab3-bundle.tsv"
 
-# starts reading from second line of the file
-remaining_lines=$(tail -n +2 lab3-bundle.csv | wc -l)
-echo $remaining_lines
+# convert the cleaned TSV file to CSV format
+tr '\t' ',' < cleaned_lab3_data.tsv > lab-bundle.csv
 
-tar -czvf lab3-bundle.csv.tar.gz converted-archive.tar.gz
+# count the number of remaining lines excluding the header
+remaining_lines=$(tail -n +2 "lab3-bundle.csv" | wc -l)
+echo "Remaining lines: $remaining_lines"
+
+# compress the CSV file into a new tar.gz archive
+tar -czvf lab3-bundle.csv.tar.gz lab3-bundle.csv 
+
+# cleanup
+rm lab3-bundle.tar.gz cleaned_lab3-bundle.tsv
+
+echo "Output archive: lab3-bundle.csv.tar.gz" 
